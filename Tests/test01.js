4f4ed3a6c965737e22c7691c493a9b3b19a490db
Richard Wilde <richard@wildesoft.net>
2014-09-29 19:19:39 +0100

    Added markdown slides, explained graby, test01 and test02

--- Tests/test01.js
+++ Tests/test01.js
@@ -1,7 +1,7 @@
 casper.test.begin('Home Page Tests', 3, function (test) {
 
-    casper.start("http://localhost:43502/", function () {
-        test.assertTitle("Home - CasperJS Mvc", "Title for home page is the one expected");
+    casper.start("http://localhost:43502", function () {
+        test.assertTitle("Home - CasperJS Mvc");
         test.assertTextExists("Casper MVC", "Casper MVC is present on page");
         test.assertTextExists("Ariya Hidayat", "Ariya Hidayat is present on page");
     });
--- Tests/test02.js
+++ Tests/test02.js
@@ -1,17 +1,16 @@
 casper.test.begin('Home and About Page', 4, function (test) {
 
-    casper.start("http://localhost:43502/", function () {
-        test.assertTitle("Home - CasperJS Mvc", "Title for home page is the one expected");
+    casper.start("http://localhost:43502", function () {
+        test.assertTitle("Home - CasperJS Mvc");
         test.assertTextExists("Casper MVC", "Casper MVC is present on page");
     });
 
     casper.thenClick('ul > li:nth-child(2) > a', function () {
         test.assertUrlMatch(/Home\/About/, "URL ends with Home/About");
-        test.assertTitle("About - CasperJS Mvc", "Title for home page is the one expected");
+        test.assertTitle("About - CasperJS Mvc");
     });
 
     casper.run(function () {
         test.done();
     });
-
 });
